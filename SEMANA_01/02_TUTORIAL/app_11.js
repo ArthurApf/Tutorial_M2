8cc056f0e6843ee3055bd0aede58462d7f126a6e
ArthurApf <99188046+ArthurApf@users.noreply.github.com>
2022-04-22 10:35:51 -0300

    Update app_11.js

--- SEMANA_01/02_TUTORIAL/app_11.js
+++ SEMANA_01/02_TUTORIAL/app_11.js
@@ -4,11 +4,11 @@ const port = 3011;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<!DOCTYPE html> <head> <title>Etapa 0 - INSTALACAO</title></head> \
+  res.end('<!DOCTYPE html> <head> <title>HeHeHe</title></head> \
               <body>\
                 <div id="main"> \
-                       <h1> Etapa 1 - INSTALACAO - Servidor Node.js </h1> \
-                       <H2> Meu servidor NODE.js funciona!</H2> </div> \
+                       <h1> se o pato perde a pata: </h1> \
+                       <H2> ele fica manco ou viuvo?</H2> </div> \
               </body> \
            </html>');
 });
