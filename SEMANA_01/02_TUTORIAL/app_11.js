92c9ce2432a5c1a5f6a3b3e619010a5d72409d3b
ArthurApf <99188046+ArthurApf@users.noreply.github.com>
2022-04-22 10:51:15 -0300

    Update app_11.js

--- SEMANA_01/02_TUTORIAL/app_11.js
+++ SEMANA_01/02_TUTORIAL/app_11.js
@@ -4,11 +4,11 @@ const port = 3011;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
-  res.end('<!DOCTYPE html> <head> <title>HeHeHe</title></head> \
+  res.end('<!DOCTYPE html> <head> <title>Curriculo</title></head> \
               <body>\
                 <div id="main"> \
-                       <h1> se o pato perde a pata: </h1> \
-                       <H2> ele fica manco ou viuvo?</H2> </div> \
+                       <h1> Arthur Prado Fraige </h1> \
+                       <H2> Eng. de Comp - Aluno do Inteli</H2> </div> \
               </body> \
            </html>');
 });
